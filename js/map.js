630d81a9718319f7ade26057852ab73183ae3fb4
Klim <klim-bury@yandex.by>
2017-08-29 13:01:29 +0300

    'kjk'

--- js/map.js
+++ js/map.js
@@ -53,10 +53,17 @@ renderPins(pinsToRendering);
 var dialog = document.querySelector('.dialog');
 var dialogPanel = document.querySelector('.dialog__panel');
 
-dialog.replaceChild(renderOffer(adTemplateArray[0]), dialogPanel);
+function replaceOffer() {
+  dialog.replaceChild(renderOffer(adTemplateArray[0]), dialogPanel);
+}
+replaceOffer();
 
+function getRandomArrayLength(array) {
+
+  return Math.floor(Math.random() * array.length);
+}
 function getAvatarNumber() {
-  var index = Math.floor(Math.random() * AVATAR_NUMBERS_ARRAY.length);
+  var index = getRandomArrayLength(AVATAR_NUMBERS_ARRAY);
   var number = AVATAR_NUMBERS_ARRAY[index];
   AVATAR_NUMBERS_ARRAY.splice(index, 1);
 
@@ -64,7 +71,7 @@ function getAvatarNumber() {
 }
 
 function getTitle() {
-  var index = Math.floor(Math.random() * TITLES_ARRAY.length);
+  var index = getRandomArrayLength(TITLES_ARRAY);
   var titleName = TITLES_ARRAY[index];
   TITLES_ARRAY.splice(index, 1);
 
@@ -127,6 +134,7 @@ function createNewPin(offer) {
   pin.className = 'pin';
   pin.style.left = offer.location.x + 'px';
   pin.style.top = offer.location.y + 'px';
+  pin.tabIndex = 0;
 
   newImg.className = 'rounded';
   newImg.src = offer.author.avatar;
@@ -148,12 +156,12 @@ function renderPins(pins) {
 
   pinsContainer.appendChild(fragment);
 }
-
+var newCard = '';
 function renderOffer(item) {
   document.getElementById('lodge-template').classList.remove('hidden');
 
   var cardTemplate = document.getElementById('lodge-template').content;
-  var newCard = cardTemplate.querySelector('.dialog__panel').cloneNode(true);
+  newCard = cardTemplate.querySelector('.dialog__panel').cloneNode(true);
   var dialogTitle = document.querySelector('.dialog__title');
 
   newCard.querySelector('.lodge__title').textContent = item.offer.title;
@@ -174,3 +182,51 @@ function renderOffer(item) {
 
   return newCard;
 }
+
+var pinMap = document.querySelector('.tokyo__pin-map');
+var offerPin = pinMap.querySelectorAll('.pin');
+var closeIcon = dialog.querySelector('.dialog__close');
+
+var openOffer = function (evt) {
+  var activePin = document.querySelector('.pin--active');
+
+  if (activePin) {
+    activePin.classList.remove('pin--active');
+  }
+
+  dialog.classList.remove('hidden');
+  evt.target.classList.add('pin--active');
+  var pinNumber = evt.srcElement.currentSrc[evt.srcElement.currentSrc.length - 5];
+  var cPN = Number(pinNumber);
+
+  replaceOffer();
+};
+
+for (var j = 0; j < offerPin.length; j++) {
+  offerPin[j].addEventListener('click', openOffer);
+}
+
+pinMap.addEventListener('keydown', function (evt) {
+
+  if (evt.keyCode === 13) {
+    openOffer(evt);
+  }
+});
+
+var closeOffer = function () {
+  var activePin = document.querySelector('.pin--active');
+
+  dialog.classList.add('hidden');
+  activePin.classList.remove('pin--active');
+};
+
+closeIcon.addEventListener('click', closeOffer);
+
+pinMap.addEventListener('keydown', function (evt) {
+
+  if (evt.keyCode === 27) {
+    closeOffer();
+  }
+});
+
+
