7f63bc1e9f174418cd1dfe0edb63582da5f44a23
Klim <klim-bury@yandex.by>
2017-09-01 20:48:49 +0300

    'пофиксил'

--- js/map.js
+++ js/map.js
@@ -293,9 +293,19 @@ selectToSync.onchange = function () {
   sync(this, 'capacity');
 };
 
+var selectCapacityToSync = document.getElementById('capacity');
+selectCapacityToSync.onchange = function () {
+  sync(this, 'room_number');
+};
+
 var timeToSync = document.getElementById('timein');
 timeToSync.onchange = function () {
   sync(this, 'timeout');
+};
+
+var timeOutToSync = document.getElementById('timeout');
+timeOutToSync.onchange = function () {
+  sync(this, 'timein');
 };
 
 
@@ -304,15 +314,18 @@ function getMinPrice(val) {
   switch (val) {
     case 'flat':
       document.getElementById('price').value = 1000;
+      document.getElementById('price').min = 1000;
       break;
     case 'bungalo':
       document.getElementById('price').value = 0;
       break;
     case 'house':
       document.getElementById('price').value = 5000;
+      document.getElementById('price').min = 5000;
       break;
     case 'palace':
       document.getElementById('price').value = 10000;
+      document.getElementById('price').min = 10000;
       break;
   }
 }
