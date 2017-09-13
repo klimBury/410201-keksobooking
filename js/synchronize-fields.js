9e4c1d287e311a7792952d3f544fb01ce3d9209c
unknown <klim-bury@yandex.by>
2017-09-13 14:40:58 +0300

    'попытка2'

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -2,9 +2,9 @@
 
 (function () {
 
-    window.synchronizeFields = function(field1, field2, fieldData1, fieldData2, cb) {
-        var indexValue = fieldData1.indexOf(field1.value);
-        cb(field2, fieldData2[indexValue]);
-    };
+  window.synchronizeFields = function (field1, field2, fieldData1, fieldData2, cb) {
+    var indexValue = fieldData1.indexOf(field1.value);
+    cb(field2, fieldData2[indexValue]);
+  };
 
 })();
