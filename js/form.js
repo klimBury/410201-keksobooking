a9a3a5deff130ba427298dc7d3fec7232e3c7652
unknown <klim-bury@yandex.by>
2017-09-14 15:36:21 +0300

    'fix'

--- js/form.js
+++ js/form.js
@@ -5,19 +5,9 @@
   var checkinTime = document.querySelector('#timein');
   var checkoutTime = document.querySelector('#timeout');
 
-  var syncValues = function (element, value) {
+  function syncValues(element, value) {
     element.value = value;
-  };
-
-  var selectToSync = document.querySelector('#room_number');
-  selectToSync.onchange = function () {
-    sync(this, 'capacity');
-  };
-
-  var selectCapacityToSync = document.querySelector('#capacity');
-  selectCapacityToSync.onchange = function () {
-    sync(this, 'room_number');
-  };
+  }
 
   checkinTime.addEventListener('change', function (evt) {
     window.synchronizeFields(evt.target, checkoutTime, window.data.time, window.data.time, syncValues);
@@ -27,29 +17,16 @@
     window.synchronizeFields(evt.target, checkinTime, window.data.time, window.data.time, syncValues);
   });
 
+  var apartmentType = document.querySelector('#type');
+  var pricePerNight = document.querySelector('#price');
 
-  function getMinPrice(val) {
-
-    switch (val) {
-      case 'flat':
-        document.querySelector('#price').value = 1000;
-        document.querySelector('#price').min = 1000;
-        break;
-      case 'bungalo':
-        document.getElementById('price').value = 0;
-        break;
-      case 'house':
-        document.getElementById('price').value = 5000;
-        document.getElementById('price').min = 5000;
-        break;
-      case 'palace':
-        document.getElementById('price').value = 10000;
-        document.getElementById('price').min = 10000;
-        break;
-    }
+  function syncValueWithMin(element, value) {
+    element.value = value;
+    element.min = value;
   }
 
-  document.getElementById('type').onchange = function () {
-    getMinPrice(this.options[this.selectedIndex].value);
-  };
+  apartmentType.addEventListener('change', function (evt) {
+    window.synchronizeFields(evt.target, pricePerNight, ['flat', 'bungalo', 'house', 'palace'], [1000, 0, 5000, 10000], syncValueWithMin);
+  });
+
 })();
